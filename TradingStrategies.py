93d192bf45819aab5e67313356329f1d8932df32
jurekhackerman123 <jurek.eisinger@t-online.de>
2023-11-24 11:49:46 +0100

    update

--- TradingStrategies.py
+++ TradingStrategies.py
@@ -35,76 +35,29 @@ def BuyAndHold(stockEvolution, timeStep):
     return buy, sell 
 
 
-def MovingAverage(stockEvolution, daysConsidered, dT, sellFactor ): 
+def MovingAverage(stock, timeStep): 
     '''
-    this function takes into account the last n days of the stock market 
+    If we notice an upwards or downwards trend in the moving average, buy (sell).
     '''
 
-    avg = 0
-
-    stepsPerDay = int( 1 / dT)
-
-    lower = False
-    buy = False
-
-    # consider last few days
-    for dayNo in range(daysConsidered): 
-
-        # sum up closing prices over days 
-        avg += stockEvolution[int( dayNo * int(1/dT) )]
-
-    avg /= daysConsidered
 
 
-    buyPrices = []
-    sellPrices = []
-
-    for i in range(len(stockEvolution)):
-
-        # update Avg
-        if i%stepsPerDay == 0: 
-
-            dayNo = int( num_days / i )
-
-            partOfArray = stockEvolution[i: (dayNo + daysConsidered) * stepsPerDay]
-
-            # calculate average over every stepsperday-th element 
-            avg = np.mean(partOfArray[:: stepsPerDay ].reshape(-1, stepsPerDay), axis=1)
 
 
-        if stockEvolution[i] < avg: 
-            lower = True
-        
-        if stockEvolution[i] >= avg and lower == True: 
-            buyPrice = stockEvolution[i]
-            buy = True
-            lower = False 
-            buyPrices.append(buyPrice)
+def ExponentialMovingAverage(stock, timeStep): 
+    '''
+    
+    '''
+    return None 
 
-        if buy == True and stockEvolution[i] < avg * sellFactor: 
-            buy = False 
-            lower = True
-            sellPrice = stockEvolution[i]
-            sellPrices.append(sellPrice)
-
-
-        # if last element -> sell 
-
-        if i == len(stockEvolution): 
-            sellPrice = stockEvolution[i]
-            sellPrices.append(sellPrice)
-
-    return buyPrices, sellPrices 
-
-
-def OtherMovingAverage(): 
+def CrossOverMovingAverage(stock, timeStep): 
     '''
     Use short term and long term average and buy if they cross 
     '''
     return None 
 
 
-def MeanReversion(): 
+def MeanReversion(stock, timeStep): 
     '''
     Calculate mean over certain timeframe, then calculate standard deviation and Z value indicating if a price is lower or higher than actual value
     
@@ -117,7 +70,7 @@ def MeanReversion():
 
 
 
-def RangeTrading(stockEvolution, dayRange, dT, rangeLimit, offset):
+def RangeTrading(stockEvolution, dayRange, dT, rangeLimit, offset, timeStep):
     '''
     if the Stock is clearly  - only shortterm!!! - between a min and a max value for the last x timesteps -> buy when high in this range 
     sell when break out of range... (some indicators exist ... )
@@ -154,7 +107,7 @@ def RangeTrading(stockEvolution, dayRange, dT, rangeLimit, offset):
     return None 
 
 
-def BreakOut(): 
+def BreakOut(stock, timeStep): 
     ''''
     Again, a range between min and max. 
     And here, if the stock breaks out above the max, buy and sell as soon as it falls again. 
@@ -162,25 +115,25 @@ def BreakOut():
 
 
 
-def IntraDayMeanReversion(): 
+def IntraDayMeanReversion(stock, timeStep): 
     return None 
 
 
-def MomentumStrategy(): 
+def MomentumStrategy(stock, timeStep): 
     '''
     Buy when 2nd derivative == 0
     '''
 
 
 
-def ReversalTrading(): 
+def ReversalTrading(stock, timeStep): 
     '''
     If it has been falling for a long time and now starts rising again -> buy 
     '''
 
 
 
-def BuyMorningSellNight(): 
+def BuyMorningSellNight(stock, timeStep): 
     '''
     Easy strategy where we buy every morning and sell every night 
     '''
@@ -188,22 +141,17 @@ def BuyMorningSellNight():
 
 
 
-def BuyAndSellRandomly(): 
+def BuyAndSellRandomly(stock, timeStep): 
     '''
     Buy at random times and sell at random times. 
     '''
     return None 
 
 
-def Scalping(timeA, timeB): 
+def Scalping(stock, timeStep, timeA, timeB): 
     '''
     stock has been rising for a certain short time (timeA)  -> buy 
     stock has been rising for a long time (timeB)           -> sell
     '''
 
-
-def CompareStrategies(stockEvolution, taxFactor): 
-    '''
-    this function compares the different trading strategies to each other
-    '''
 
