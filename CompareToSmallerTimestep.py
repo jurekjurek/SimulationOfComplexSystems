31a20cfc580a8e7ab3ce02901a08a7f254a55a76
jurekhackerman123 <jurek.eisinger@t-online.de>
2023-12-04 11:29:56 +0100

    update

--- CompareToSmallerTimestep.py
+++ CompareToSmallerTimestep.py
@@ -4,63 +4,69 @@ from AgentClass import *
 
 
 # global variables 
-NUMBEROFDAYS = 100
+NUMBEROFDAYS = 10
 
 INITIALPRICE = 100  # Initial stock price
 DRIFT = 0.0001       # Drift term
 VOLATILITY = 0.01  # Volatility term
 NUMBEROFSIMULATIONS = 5  # Number of simulations
 
-DT = 1/24
+# update every minute
+DT = 1/(24*60)
 
 # initial money for agent 
 INITIALMONEY = 1000
 
 def MainSimulation(initialMoney, tradingStrategy, show): 
 
-    stock = GenerateStocks(INITIALPRICE, DRIFT, VOLATILITY, NUMBEROFDAYS, DT)
+    # stock with update every minute
+    stockLowerDt = GenerateStocks(INITIALPRICE, DRIFT, VOLATILITY, NUMBEROFDAYS, DT)
 
-    # define agent 
-    agent = Agent(initialMoney, tradingStrategy, stock, holdsAtOnce=5)
+    # stock with different dT - only once every hour
+    stockHigherDt = stock[::60]
 
-    print('how much money does this guy have???', initialMoney)
+    stockList = [stockLowerDt, stockHigherDt]
 
-    for timeStep in range(len(stock)): 
+    for stock in stockList:
 
-        agent.take_action(timeStep, stock)
+        # define agent 
+        agent = Agent(initialMoney, tradingStrategy, stock, holdsAtOnce=5)
 
-    # if the end of the stock is reached and the agent is still holding stocks, sell all the stocks for the current price 
-    while len(agent.sellList) < len(agent.buyList): 
-        agent.sell(stock, timeStep, agent.taxFactor)
+        print('how much money does this guy have???', initialMoney)
 
-    print(agent.buyList)
-    print(agent.sellList)
+        for timeStep in range(len(stock)): 
 
-    if show: 
-        for i in range(len(agent.buyList)):
-            plt.scatter(agent.buyList[i], stock[agent.buyList[i]], s= 200, color = 'red')#, label = 'Buy')
-            plt.scatter(agent.sellList[i], stock[agent.sellList[i]], s=200, color = 'green')#, label = 'Sell')
-        ShowStock(stock, False, 0, False, [5, 20], NUMBEROFDAYS, DT, 'Profit: ' + str(agent.money - initialMoney))
-        # plt.plot(stock)
-        # plt.legend()
-        plt.show()
-    
-    print('current money after trading: ', agent.money)
+            agent.take_action(timeStep, stock)
+
+        # if the end of the stock is reached and the agent is still holding stocks, sell all the stocks for the current price 
+        while len(agent.sellList) < len(agent.buyList): 
+            agent.sell(stock, timeStep, agent.taxFactor)
+
+        print(agent.buyList)
+        print(agent.sellList)
+
+        if show: 
+            for i in range(len(agent.buyList)):
+                plt.scatter(agent.buyList[i], stock[agent.buyList[i]], s= 200, color = 'red')#, label = 'Buy')
+                plt.scatter(agent.sellList[i], stock[agent.sellList[i]], s=200, color = 'green')#, label = 'Sell')
+            ShowStock(stock, False, 0, False, [5, 20], NUMBEROFDAYS, DT, 'Profit: ' + str(agent.money - initialMoney))
+            # plt.plot(stock)
+            # plt.legend()
+            plt.show()
+        
+        print('current money after trading: ', agent.money)
 
 
 # MainSimulation(INITIALMONEY, BreakOut, True)
 
 
 
-def CompareTradingStrategies(initialMoney, holdsAtOnce, numberOfAverages): 
-
+def CompareTradingStrategies(stockList, initialMoney, holdsAtOnce, numberOfAverages): 
+    '''
+    We only use the short term trading strategies here. 
+    '''
     # one stock (for now, only one) that all agents with all different trading strategies will follow 
-    globalStock = GenerateStocks(INITIALPRICE, DRIFT, VOLATILITY, NUMBEROFDAYS, DT)
-
-    stockList = []
-    for i in range(numberOfAverages): 
-
-        stockList.append(GenerateStocks(INITIALPRICE, DRIFT, VOLATILITY, NUMBEROFDAYS, DT))
+    # globalStock = GenerateStocks(INITIALPRICE, DRIFT, VOLATILITY, NUMBEROFDAYS, DT)
 
     # all the trading strategies
     namesList = ['BuyAndHold', 'MovingAverage', 'Crossover', 'MeanReversion', 'Rangetrading', 'BO', 'MorningNight', 'Scalping', 'Random'] 
@@ -148,7 +154,27 @@ def CompareTradingStrategies(initialMoney, holdsAtOnce, numberOfAverages):
     plt.show()
 
 
-CompareTradingStrategies(1000, 5, 100)
+NUMBEROFAVERAGES = 5
 
+# generate the two stocklists 
+stockListSmallDt = []
+stockListHighDt = []
+
+for i in range(NUMBEROFAVERAGES): 
+    DT = 1/(24*60)
+
+    # every minute
+    stockListSmallDt.append(GenerateStocks(INITIALPRICE, DRIFT, VOLATILITY, NUMBEROFDAYS, DT))
+
+    DT = 1/24
+
+    # every hour
+    stockListHighDt.append(stockListSmallDt[i][::60])
+
+
+
+
+CompareTradingStrategies(stockListSmallDt ,1000, 1, NUMBEROFAVERAGES)
+CompareTradingStrategies(stockListHighDt ,1000, 1, NUMBEROFAVERAGES)
 
 
